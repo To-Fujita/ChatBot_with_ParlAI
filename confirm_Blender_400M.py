c4937c6166a5f24ffc202db8a6fca855edb3aed6
T. Fujita <53250813+To-Fujita@users.noreply.github.com>
2022-05-05 11:03:08 +0900

    Update confirm_Blender_400M.py

--- confirm_Blender_400M.py
+++ confirm_Blender_400M.py
@@ -1,7 +1,8 @@
 # Confirm ParlAI Agent for Blender_400M by T. Fujita 2021/08/01
 
 import sys
-sys.path.append('./ParlAI-master/')
+#sys.path.append('./ParlAI-master/')
+sys.path.append('./ParlAI-main/')
 
 from parlai.scripts.interactive import Interactive
 
