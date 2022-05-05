3d71cab696dcf0ef3310fa7001e07a18c76e00c1
T. Fujita <53250813+To-Fujita@users.noreply.github.com>
2022-05-05 11:01:18 +0900

    Update confirm_Alice.py

--- confirm_Alice.py
+++ confirm_Alice.py
@@ -1,7 +1,7 @@
 # Confirm ParlAI Agent for Alice by T. Fujita 2021/08/01
 
 import sys
-sys.path.append('./ParlAI-master/')
+#sys.path.append('./ParlAI-master/')
 sys.path.append('./ParlAI-main/')
 
 from parlai.scripts.interactive import Interactive
