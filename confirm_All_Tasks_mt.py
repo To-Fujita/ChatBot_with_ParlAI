7a3dce7333388604c14d51aaca52276852542bcc
T. Fujita <53250813+To-Fujita@users.noreply.github.com>
2022-05-05 11:02:23 +0900

    Update confirm_All_Tasks_mt.py

--- confirm_All_Tasks_mt.py
+++ confirm_All_Tasks_mt.py
@@ -1,7 +1,8 @@
 # Confirm ParlAI Agent for All_Tasks_mt by T. Fujita 2021/08/01
 
 import sys
-sys.path.append('./ParlAI-master/')
+#sys.path.append('./ParlAI-master/')
+sys.path.append('./ParlAI-main/')
 
 from parlai.scripts.interactive import Interactive
 
